61e4cc2d8a055282712458e2c16e3bc2e5e55d43
Leandro-DevPT <lr26122008@gmail.com>
2025-09-21 15:04:54 +0100

    Add files via upload

--- projeto3.py
+++ projeto3.py
@@ -32,5 +32,5 @@ def home():
     return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    webbrowser.open("http://127.0.0.1:5000")
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+
